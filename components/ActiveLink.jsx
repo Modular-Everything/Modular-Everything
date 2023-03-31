b91a892be6fbb8e1ed83ca78b84e8b3874720b95
Chrish Dunne <yo@modular-everything.com>
2023-03-31 15:02:30 +0100

    feat: Added ActiveLink component

---
+++ components/ActiveLink.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-globals */
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+export function ActiveLink({
+  children,
+  activeClassName = "active",
+  className,
+  ...props
+}) {
+  const { asPath, isReady } = useRouter();
+  const [computedClassName, setComputedClassName] = useState(className);
+
+  useEffect(() => {
+    if (isReady) {
+      const linkPathname = new URL(props.as || props.href, location.href)
+        .pathname;
+
+      const activePathname = new URL(asPath, location.href).pathname;
+
+      const newClassName =
+        linkPathname === activePathname
+          ? `${className} ${activeClassName}`.trim()
+          : className;
+
+      if (newClassName !== computedClassName) {
+        setComputedClassName(newClassName);
+      }
+    }
+
+    console.log(computedClassName);
+  }, [
+    asPath,
+    isReady,
+    props.as,
+    props.href,
+    activeClassName,
+    className,
+    computedClassName,
+  ]);
+
+  return (
+    <Link className={computedClassName} {...props}>
+      {children}
+    </Link>
+  );
+}
