7cb4705de7ea36a4e1b3576d6478d209398b30ed
Chrish Dunne <yo@modular-everything.com>
2023-05-10 09:25:27 +0100

    feat: Added temporary description

--- components/Head.jsx
+++ components/Head.jsx
@@ -4,6 +4,10 @@ export function PageHead({ title }) {
   return (
     <Head>
       <title>{title}</title>
+      <meta
+        name="description"
+        content="Freelance creative web developer working with NextJS, React and dabbling lately in GSAP."
+      />
     </Head>
   );
 }
